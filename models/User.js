4619b00e9f5c0446e8b768c95642052b0a7abbad
Flash2022 <tarunsingh18bcs@iiitkottayam.ac.in>
2021-04-13 23:26:53 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    googleId:{
+        type: String,
+        required: true
+    },
+    displayName:{
+        type: String,
+        required: true
+    },
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
+        type: String
+        
+    },
+    Image:{
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('User',UserSchema)
+
