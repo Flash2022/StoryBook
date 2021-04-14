56066e6dac9c60c321371b899208e042092a5173
Tarun Singh <82393306+Flash2022@users.noreply.github.com>
2021-04-14 13:47:34 +0530

    Update stories.js

--- route/stories.js
+++ route/stories.js
@@ -1,46 +1,44 @@
 const express = require('express')
 const router = express.Router()
-const {ensureAuth } = require('../middleware/auth')
+const { ensureAuth } = require('../middleware/auth')
 
 const Story = require('../models/Story')
-// @desc      show add page
-//@route      GET /stories/add
-router.get('/add' , ensureAuth, (req,res) =>{
-    res.render('stories/add')
+
+// @desc    Show add page
+// @route   GET /stories/add
+router.get('/add', ensureAuth, (req, res) => {
+  res.render('stories/add')
 })
 
-// @desc      process add form
-//@route      post /stories
-router.post('/' , ensureAuth, async(req,res) =>{
-   try {
-       req.body.user = req.user.id
-       await Story.create(req.body)
-       res.redirect('/dashboard')
-       } catch (err) {
-       console.error(err)
-       res.render('error/500')
-   }
+// @desc    Process add form
+// @route   POST /stories
+router.post('/', ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id
+    await Story.create(req.body)
+    res.redirect('/dashboard')
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
+// @desc    Show all stories
+// @route   GET /stories
+router.get('/', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ status: 'public' })
+      .populate('user')
+      .sort({ createdAt: 'desc' })
+      .lean()
 
-// @desc      show all stories
-//@route      GET /stories/add
-router.get('/' , ensureAuth, async(req,res) =>{
-    try {
-        const stories = await Story.find({status: 'public'})
-        .populate('user')
-        .sort({createdAt: 'desc'})
-        .lean()
-
-        res.render('stories/index',{
-            stories
-        })
-        
-    } catch (err) {
-        console.error(err)
-        res.render('error/500')
-        
-    }
+    res.render('stories/index', {
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
 // @desc    Show single story
@@ -69,27 +67,27 @@ router.get('/:id', ensureAuth, async (req, res) => {
 // @desc    Show edit page
 // @route   GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-    try {
-      const story = await Story.findOne({
-        _id: req.params.id,
-      }).lean()
-  
-      if (!story) {
-        return res.render('error/404')
-      }
-  
-      if (story.user != req.user.id) {
-        res.redirect('/stories')
-      } else {
-        res.render('stories/edit', {
-          story,
-        })
-      }
-    } catch (err) {
-      console.error(err)
-      return res.render('error/500')
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean()
+
+    if (!story) {
+      return res.render('error/404')
     }
-  })
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      res.render('stories/edit', {
+        story,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
+})
 
 // @desc    Update story
 // @route   PUT /stories/:id
